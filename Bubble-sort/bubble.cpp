f9ce9e79daeca166ae68166cd993ec2456c33f47
Gabriel Minatel <gabriel.minatel71@gmail.com>
2026-06-23 09:14:38 -0300

    commentaries & optimization to bubble sort

--- Bubble-sort/bubble.cpp
+++ Bubble-sort/bubble.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 int main() {
-    int arr[10], i, j, n, cont;
+    int arr[10], i, j, n;
 
     cout << "Inform what is array's length = ";
     cin >> n;
@@ -14,11 +14,11 @@ int main() {
     cout << endl;
     
     cout << "Sorted Array: ";
-    for(i = 1; i < n; i++) 
+    for(i = 1; i < n - 1; i++) // it passes values that are already sorted, decrease processing
     {
-        for(j = 0; j < n - i; j++) 
+        for(j = 0; j < n - i; j++) // inner loop: shrinks each pass 
         {
-            if(arr[j] > arr[j+1]) 
+            if(arr[j] > arr[j+1]) // Swap values when the current position is bigger than position ahead
             {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
